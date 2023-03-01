5adb51adf4ad0c271a753621553b94b6b3a5acb9
BrendanLeeGit <brendanlee09090@gmail.com>
2023-02-28 19:43:54 -0500

    Next design pattern is model view controller, wrote down notes

---
+++ src/MVC/Notes.java
@@ -0,0 +1,13 @@
+package MVC;
+
+public class Notes {
+    /*
+    USER uses CONTROLLER manipulates MODEL updates VIEW displaysTo USER
+    the assignment
+    Learn Swing
+    use the program for csv into mvc, but map shit to swing components
+    have text box that displays stuff, buttons with clear,
+    button with plot and export
+    controls in its own world, data in its own world, and display in its own world
+     */
+}
