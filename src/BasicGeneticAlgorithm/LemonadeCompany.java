6dd5f645488000beb2ec6f2f7ed7ecbef53d24dd
BrendanLeeGit <brendanlee09090@gmail.com>
2023-04-14 00:12:07 -0400

    Started genetic alg funsies project

---
+++ src/BasicGeneticAlgorithm/LemonadeCompany.java
@@ -0,0 +1,5 @@
+package BasicGeneticAlgorithm;
+
+public class LemonadeCompany {
+
+}
--- src/BasicGeneticAlgorithm/LemonadeStand.java
+++ src/BasicGeneticAlgorithm/LemonadeStand.java
@@ -1,4 +1,41 @@
 package BasicGeneticAlgorithm;
 
 public class LemonadeStand {
+    private int quantityOfProduct;
+    private double qualityOfProduct;
+    private int possibleBuyers;
+    private double percentageOfCustomersBuying;
+    private int[] moneyAllocation;
+    private double profit;
+
+    public LemonadeStand(int[] moneyAllocation){
+        /*
+        The indexes of moneyAllocation are as follows:
+        0 - $ spent on quantity of product
+        1 - $ spent on quality of product
+        3 - $ spent on advertisement for product
+        4 - Price of product
+         */
+        quantityOfProduct = moneyAllocation[0] * 2;
+        qualityOfProduct = moneyAllocation[1] * 0.1;
+        possibleBuyers = moneyAllocation[2] * 2;
+
+        //Percentage of customers buying is calculated by
+        //(quality's price)/(actual price)
+        percentageOfCustomersBuying = qualityOfProduct/moneyAllocation[3];
+        this.moneyAllocation = moneyAllocation;
+        profit = 0;
+    }
+
+    public void calculateProfit(){
+        profit = quantityOfProduct * possibleBuyers * percentageOfCustomersBuying;
+    }
+
+    public double getProfit(){
+        return profit;
+    }
+
+    public int[] getParams(){
+        return moneyAllocation;
+    }
 }
